f10ea63dcad7b40f0f7946f8fd69c8ee6565897a
JR-Redius <kanev.ant@gmail.com>
2021-09-22 16:26:51 +0300

    1620229

--- bot_aiogram/bot_new.py
+++ bot_aiogram/bot_new.py
@@ -1,10 +1,61 @@
+import logging
 from typing import Text
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
+
+
+bot = Bot(token="1966342656:AAEikQMfVsuHureW2I8fJhPJtx4zG64rVvc")
+dp=Dispatcher(bot)
+
+logging.basicConfig(level=logging.INFO)
+
+# Здесь хранятся пользовательские данные.
+# Т.к. это словарь в памяти, то при перезапуске он очистится
+user_data = {}
+
 def get_keyboard():
+    # Генерация клавиатуры.
     buttons = [
-        types.InlineKeyboardButton(text='Изменить пароль', callback_data='edit_secret'),
-        types.InlineKeyboardButton(text='Изменить имя', callback_data='edit_fullname')
+        types.InlineKeyboardButton(text="-1", callback_data="num_decr"),
+        types.InlineKeyboardButton(text="+1", callback_data="num_incr"),
+        types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish")
     ]
-    
+    # Благодаря row_width=2, в первом ряду будет две кнопки, а оставшаяся одна
+    # уйдёт на следующую строку
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    return keyboard
+
+async def update_num_text(message: types.Message, new_value: int):
+    # Общая функция для обновления текста с отправкой той же клавиатуры
+    await message.edit_text(f"Укажите число: {new_value}", reply_markup=get_keyboard())
+
+@dp.message_handler(commands="numbers")
+async def cmd_numbers(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard())
+
+@dp.callback_query_handler(lambda c: c.data.startswith('num_'))
+async def callbacks_num(call: types.CallbackQuery):
+    # Получаем текущее значение для пользователя, либо считаем его равным 0
+    user_value = user_data.get(call.from_user.id, 0)
+    # Парсим строку и извлекаем действие, например `num_incr` -> `incr`
+    action = call.data.split("_")[1]
+    if action == "incr":
+        user_data[call.from_user.id] = user_value+1
+        await update_num_text(call.message, user_value+1)
+    elif action == "decr":
+        user_data[call.from_user.id] = user_value-1
+        await update_num_text(call.message, user_value-1)
+    elif action == "finish":
+        # Если бы мы не меняли сообщение, то можно было бы просто удалить клавиатуру
+        # вызовом await call.message.delete_reply_markup().
+        # Но т.к. мы редактируем сообщение и не отправляем новую клавиатуру, 
+        # то она будет удалена и так.
+        await call.message.edit_text(f"Итого: {user_value}")
+    # Не забываем отчитаться о получении колбэка
+    await call.answer()
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
